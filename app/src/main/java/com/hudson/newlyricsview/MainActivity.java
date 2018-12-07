9a006a9954511d1ebf1a8d4c97c73fc41120f85b
hudson <221627@nd.com>
2018-12-07 15:02:12 +0800

    使用

--- app/src/main/java/com/hudson/newlyricsview/MainActivity.java
+++ app/src/main/java/com/hudson/newlyricsview/MainActivity.java
@@ -2,6 +2,9 @@ package com.hudson.newlyricsview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.hudson.newlyricsview.lyrics.LyricsController;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View lyricsView = new LyricsController(null, this).getLyricsView();
     }
 }
